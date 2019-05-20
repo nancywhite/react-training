1b5b985e595e93af27431d568c5a879bfac887bb
tsid14 <tsid14@lm.lmig.com>
2019-05-20 16:25:14 -0400

    added karma

--- spec/sayHelloSpec.js
+++ spec/sayHelloSpec.js
@@ -1,15 +1,27 @@
 let sayHello = require('../src/sayHello.js');
 
 describe('Greet', function() {
-    it('concats Hello and a name', function() {
+    it('concats Hello and a name when a name is given', function() {
         const actual = sayHello.greet('Nancy');
-        const expected = 'Hello Nancy!';
+        const expected = 'Hello Nancy';
         expect(actual).toEqual(expected);
     });
 
-    it('says Hello whoever you are! when no name is given', function() {
+    it('concats Hola and a name for Spanish speakers when a name is given', function() {
+        const actual = sayHello.greet('Maria', 'Spanish');
+        const expected = 'Hola Maria';
+        expect(actual).toEqual(expected);
+    });
+
+    it('says Hello friend when no name is given', function() {
         const actual = sayHello.greet();
-        const expected = 'Hello whoever you are!';
+        const expected = 'Hello friend';
+        expect(actual).toEqual(expected);
+    });
+
+    it('says Hola amigo when no name is given and preferred language is Spanish', function() {
+        const actual = sayHello.greet(undefined, 'Spanish');
+        const expected = 'Hola amigo';
         expect(actual).toEqual(expected);
     });
 });
--- src/sayHello.js
+++ src/sayHello.js
@@ -1,3 +1,16 @@
-exports.greet = function greet(name = 'whoever you are') {
-    return `Hello ${name}!`;
+exports.greet = function greet(name, preferredLanguage = 'English') {
+    let greeting = 'Hello';
+    let friend = 'friend';
+
+    switch (preferredLanguage) {
+    case 'Spanish':
+        greeting = 'Hola';
+        friend = 'amigo';
+        break;
+    default:
+        break;
+    }
+
+    let who = name ? name: friend;
+    return `${greeting} ${who}`;
 };
